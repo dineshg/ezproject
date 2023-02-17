0ec4bbf5a5d10c8fc937e749c4f661fbbfd393fa
Dinesh Gamage <dinesh.srikantha@gmail.com>
2023-02-17 14:05:17 +1100

    making a bit advanced

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!"
+    return 'Hello, World!'
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        user_data = request.form
+        # Do something with user_data, such as storing it in a database
+        return 'Data submitted successfully!'
+    else:
+        return '''
+        <form method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name"><br>
+            <label for="email">Email:</label>
+            <input type="email" name="email"><br>
+            <button type="submit">Submit</button>
+        </form>
+        '''
+
+if __name__ == '__main__':
+    app.run()
