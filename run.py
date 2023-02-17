55a19b954385dd2fde84a213289fc83a027e9d9f
Dinesh Gamage <dinesh.srikantha@gmail.com>
2023-02-17 14:18:17 +1100

    new

--- run.py
+++ run.py
@@ -1,27 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
         user_data = request.form
         # Do something with user_data, such as storing it in a database
-        return 'Data submitted successfully!'
+        return render_template('thanks.html')
     else:
-        return '''
-        <form method="post">
-            <label for="name">Name:</label>
-            <input type="text" name="name"><br>
-            <label for="email">Email:</label>
-            <input type="email" name="email"><br>
-            <button type="submit">Submit</button>
-        </form>
-        '''
+        return render_template('form.html')
 
 if __name__ == '__main__':
     app.run()
